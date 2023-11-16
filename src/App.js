1e0efd610fdbf0397aad8be485c91b8d3575ba7f
kadhir <kkadhir243@gmail.com>
2023-11-16 15:41:26 +0530

    stcommit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html>
+    <h1>welcome to website</h1>
+    <p1>iam kadhiravan</p1>
+    </html>
   );
 }
 
